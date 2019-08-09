a09751e9ff8ca4c4e9d1e79a87bd7e7c19806387
wkfenrir <wkfenrir@gmail.com>
2019-08-09 12:58:38 -0700

    read email second method

---
+++ ReadEmailY.py
@@ -0,0 +1,16 @@
+import email ​
+import imapclient 
+from imapclient import IMAPClient
+​
+HOST = 'imap.gmail.com'
+USERNAME = 'bridgetorpy@gmail.com'
+PASSWORD = 'khorma12'
+​
+with IMAPClient(HOST) as server:
+    server.login(USERNAME, PASSWORD)
+    server.select_folder('INBOX', readonly=True)
+​
+    messages = server.search()
+    for uid, message_data in server.fetch(messages, 'RFC822').items():
+        email_message = email.message_from_bytes(message_data[b'RFC822'])
+        print(uid, email_message.get_payload())
